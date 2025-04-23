31cfe8d2e2512dfbaceb1dbcf773bf39fbc5090a
Joshua de wet <ST10313014@vcconnect.edu.za>
2025-04-23 17:54:08 +0200

    INITIAL COMMIT

---
+++ app/src/main/java/com/jakt/jaktprog7313budgetbeaters/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.jakt.jaktprog7313budgetbeaters
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+
+        // here goes the splash screen. this stuff will come later once core functionality is implemented
+
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
